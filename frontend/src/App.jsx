f2963afa7e201adc14ae53b6d996c4aebceeee08
AbouAmjad <ma7moud.iskandr@gmail.com>
2025-11-15 10:21:13 +0300

    Change router from HashRouter to BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './Login'
 import Scanner from './Scanner'
 import Inventory from './Inventory'
